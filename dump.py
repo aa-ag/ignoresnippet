9912e651c1c6c6f04b68f2901725fc058bb33fcd
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-24 14:51:45 -0600

    refactored, tested, working

--- dump.py
+++ dump.py
@@ -1,33 +1,46 @@
 ###--- IMPORTS ---###
 import json
+import settings
+import os
+
+
+###--- GLOBAL VARIABLES ---###
+path_to_vsc_codesnippets = settings.CODE_SNIPPET_PATH
+
 
 ###--- CODE-SNIPPET BUILDER ---###
 
-# [!] optiona: accetps input
-# name = input("*** Enter a name for your code-snippet: ")
-# prefix = input("\n*** Enter a prefix for your code-snippet: ")
-name = "gitignore"
-prefix = "ignore"
-description = "A gitignore template for Python projects."
+def create_codesnippet():
+    # [!] optional: accetps input
+    # name = input("*** Enter a name for your code-snippet: ")
+    # prefix = input("\n*** Enter a prefix for your code-snippet: ")
+    name = "gitignore"
+    prefix = "ignore"
+    description = "A gitignore template for Python projects."
 
-body = list()
-with open('template.txt') as doc:
-    for line in doc:
-        body.append(line.replace('\n', ''))
+    body = list()
+    with open('template.txt') as doc:
+        for line in doc:
+            body.append(line.replace('\n', ''))
 
-code_snippet = dict()
+    code_snippet = dict()
 
-code_snippet[name] = {
-    "prefix": prefix,
-    "body": body,
-    "description": description,
-}
+    code_snippet[name] = {
+        "prefix": prefix,
+        "body": body,
+        "description": description,
+    }
 
-print(type(code_snippet))  # dict
-# print(code_snippet)
+    # print(type(code_snippet))  # dict
+    # print(code_snippet)
 
-snippet_as_json = json.dumps(code_snippet, indent=4)
-print(type(snippet_as_json))  # str
-print(snippet_as_json)
+    snippet_as_json = json.dumps(code_snippet, indent=4)
+    # print(type(snippet_as_json))  # str
 
-# TO DO: set as .code-snippets on VSC
+    with open(os.path.join(path_to_vsc_codesnippets, f"{name}.code-snippets"), "w") as final_snippet:
+        final_snippet.write(snippet_as_json)
+
+
+###--- DRIVER CODE ---###
+if __name__ == "__main__":
+    create_codesnippet()
---
+++ settings.py
@@ -0,0 +1,5 @@
+# https://code.visualstudio.com/docs/editor/userdefinedsnippets
+# https://snippet-generator.app/
+# https://github.com/pawelgrzybek/snippet-generator/blob/master/src/components/VSCode.js
+
+CODE_SNIPPET_PATH = '/Users/aaronaguerrevere/Library/Application Support/Code/User/snippets'
