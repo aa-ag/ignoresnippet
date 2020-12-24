432d5c1a7bcbd68d8c1bdfe86421c215a37422e3
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-24 15:01:02 -0600

    rmd settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-# https://code.visualstudio.com/docs/editor/userdefinedsnippets
-# https://snippet-generator.app/
-# https://github.com/pawelgrzybek/snippet-generator/blob/master/src/components/VSCode.js
-
-CODE_SNIPPET_PATH = '/Users/aaronaguerrevere/Library/Application Support/Code/User/snippets'
